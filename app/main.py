9f58ac4fb6ca6031e4f57aa4d516230a4ba806b8
footing <bmyhack@qq.com>
2025-02-11 10:26:19 +0800

    2

--- app/main.py
+++ app/main.py
@@ -1,19 +1,26 @@
+# 标准库导入
+from typing import List
+
+# 第三方库导入
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from .routers import item_router,node_router
 
+# 应用程序内部模块导入
+from .routers import item_router, node_router
+
+# 创建FastAPI应用实例
 app = FastAPI()
 
 # 挂载静态文件目录
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
+# 包含路由
 app.include_router(item_router.router)
 app.include_router(node_router.router)
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
-
 
 if __name__ == "__main__":
     import uvicorn
